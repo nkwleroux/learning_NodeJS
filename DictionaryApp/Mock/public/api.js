0d1a761b7c460b07e3af4901d12f3a2fd15191f4
Nicholas <nkw.leroux@student.avans.nl>
2022-03-26 22:17:50 +0100

    WIP Dictionary app
    Playing with css

--- DictionaryApp/Mock/public/api.js
+++ DictionaryApp/Mock/public/api.js
@@ -8,31 +8,69 @@ let audio;
 const wordsList = wrapper.querySelector(".wordsList .list");
 const synonyms = wrapper.querySelector(".synonyms .list");
 const antonyms = wrapper.querySelector(".antonyms .list");
+let list = [];
 
 //debug (calls onLoad + my own funciton)
 document.addEventListener("DOMContentLoaded", function () {
   if (searchInput.value == undefined || searchInput.value == "") {
     //Debug only
+    for (let index = 0; index < 15; index++) {
+      let tag = `<span>- ${index}</span>`;
+      const sidebar = document.querySelector(".sidebar .list");
+      sidebar.insertAdjacentHTML("beforeend", tag);
+    }
+
+    
     fetchApi("set");
     return;
   }
   fetchApi(searchInput.value);
 });
 
-function data(result, word) {
+function data(result) {
   wrapper.classList.add("active");
+
+  //if i want the whole word search result
+  // let wordInput = {
+  //   Word: result[0].word,
+  //   PartOfSpeech: result[0].meanings[0].partOfSpeech,
+  //   Phonetics: result[0].phonetics[0].text,
+  //   Meanings: result[0].meanings[0].definitions,
+  //   Synonyms: result[0].meanings[0].synonyms,
+  //   Antonyms: result[0].meanings[0].antonyms,
+  // };
+
+  // let jsOb = JSON.stringify(wordInput); //to json
+  // let parsedata = JSON.parse(jsOb); // from json
+
+  // if (!list.includes(jsOb)) {
+  //   list.push(jsOb);
+  // }
 
   //Word
   let wordSearched = result[0].word.replace(/^\w/, (c) => c.toUpperCase());
   document.querySelector(".word strong").innerText = wordSearched;
-  console.log(`${wordSearched}`);
+
+  if (!list.includes(result[0].word)) {
+    list.push(result[0].word);
+    let tag = `<span>- ${wordSearched}</span>`;
+    const sidebar = document.querySelector(".sidebar .list");
+    sidebar.insertAdjacentHTML("beforeend", tag);
+  }
 
   //Phonetics
   let phontetics = "";
-  if (result[0].phonetics[0] != undefined) {
+  if (result[0].phonetics[0].text != undefined) {
     phontetics = `${result[0].meanings[0].partOfSpeech}  ${result[0].phonetics[0].text}`;
   }
   document.querySelector(".word span").innerText = phontetics;
+
+  if (result[0].phonetics[0].text != undefined) {
+    volume.style.display = "block";
+    audio = new Audio(result[0].phonetics[0].audio);
+  } else {
+    volume.style.display = "none";
+  }
 
   //Words
   wordsList.innerHTML = "";
@@ -60,7 +98,6 @@ function data(result, word) {
 
 //synonyms & antonyms
 function addLinkedRelatedWords(html, wordsList) {
-  console.log(wordsList[0]);
   let tag;
   if (wordsList[0] == undefined) {
     html.parentElement.style.display = "none";
@@ -92,7 +129,7 @@ function fetchApi(word) {
       throw new Error("Failed getting JSON response");
     })
     .then((responseJSON) => {
-      data(responseJSON, word);
+      data(responseJSON);
     })
     .catch((err) => {
       console.log(err);
@@ -109,7 +146,7 @@ searchInput.addEventListener("keyup", (e) => {
 //TODO Audio doesn't work
 volume.addEventListener("click", () => {
   volume.style.color = "#4D59FB";
-  // audio.play();
+  audio.play();
   setTimeout(() => {
     volume.style.color = "#999";
   }, 800);
@@ -122,3 +159,33 @@ removeIcon.addEventListener("click", () => {
   infoText.style.color = "#9A9A9A";
   infoText.innerHTML = "";
 });
+
+//----------------//
+
+//for "function data"
+//!bit overkill and scuffed
+//!Prints a list of all info of a word
+// let ob = result.map((object) => {
+//   const { word, phonetics, meanings } = object;
+
+//   let phoneticsText = phonetics.map((phoneticsOb) => {
+//     const { text } = phoneticsOb;
+//     return { text };
+//   });
+
+//   let meaningsText = meanings.map((meaningsOb) => {
+//     const { partOfSpeech, definitions, synonyms, antonyms } = meaningsOb;
+
+//     let defAndExText = definitions.map((defAndExOb) => {
+//       const { definition, example } = defAndExOb;
+//       return { definition, example };
+//     });
+
+//     return { partOfSpeech, defAndExText, synonyms, antonyms };
+//   });
+
+//   return { word, phoneticsText, meaningsText };
+// });
+// console.log(ob);
+
+//----------------//
--- StarterApp/final/JSONintro.js
+++ StarterApp/final/JSONintro.js
@@ -5,17 +5,64 @@ const { products } = require("../data");
 app.get("/", (req, res) => {
   //To print out json to server
   //res.json([{ name: "test" }, { name: "person" },products]);
-  res.send('<h1>Home page</h1><a href="/api/products">products</a>')
+  res.send('<h1>Home page</h1><a href="/api/products">products</a>');
 });
 
-app.get('/api/products',(req,res)=>{
-  const newProducts = products.map((product)=>{
-    const {id,name,image} = product;
-    return {id,name,image}
-  })
+//for all products
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
   res.json(newProducts);
+});
 
-})
+//for specific product (dynamically add html pages)
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist.");
+  }
+  res.json(singleProduct);
+});
+
+//can use :productID and :reviewID as paramaters
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  // const { productID } = req.params;
+  // const singleProduct = products.find((product) => product.id === Number(productID));
+  // if(!singleProduct){
+  //   return res.status(404).send('Product does not exist.')
+  // }
+  // res.json(singleProduct);
+});
+
+//coding standard
+//query for search?= ...
+//params for :...
+app.get("/api/v1/query", (req, res) => {
+  console.log(`searching ${req.query}`);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+  console.log(sortedProducts);
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if(sortedProducts.length <1){
+    //example
+    return res.status(200).json({success:true,data:[sortedProducts]})
+  }
+  res.status(200).json(sortedProducts);
+});
 
 app.listen(5000, () => {
   console.log("started");
